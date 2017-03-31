e05be5065e292fb2d8bd8e767a53b864199a9e90
promethwill88 <wfong.bk@gmail.com>
2017-03-30 21:28:55 -0700

    added event listeners

--- starter-code/change/change.js
+++ starter-code/change/change.js
@@ -4,4 +4,20 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
+  $('#left').on('change', inputLeft);
+
+  function inputLeft(event){
+  	var $leftValue = $('#left').val();
+  }
+
+  $('#right').on('change', inputRight);
+
+  function inputRight(event){
+  	var $rightValue = $('#right').val();
+  }
+
+
+  let result = $leftValue + $rightValue;
+  $('#total').val(result);
 })
+// Checked solution, gave up
