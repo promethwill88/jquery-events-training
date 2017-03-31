b3f4f015b613a48ae8527bcbfd858030a5047889
promethwill88 <wfong.bk@gmail.com>
2017-03-30 18:49:16 -0700

    moved ready method close bracket

--- starter-code/ready/ready.js
+++ starter-code/ready/ready.js
@@ -4,13 +4,18 @@
 
 $(document).ready(function(){
   console.log( "The page says:", $("h1").text() );
-});
 
+
+// Selects h1 and change to "Go!""
 $("h1").text("Go!");
 
-if ( $("h1").text() === "Go!" ){
-  console.log( "The page says:", $("h1").text() );
-  console.log("Success!");
+if ( $("h1").text() === "Ready?" ){
+  	console.log( "The page says:", $("h1").text() );
+  	console.log("Success!");
+// Currently, failing to else
 } else {
-  console.log( 'Can you make it say "Go!"?' );
+ 	console.log( 'Can you make it say "Go!"?' );
+  
 }
+});
+//changed bracket to here from line 7 to include code AFTER page loads.
